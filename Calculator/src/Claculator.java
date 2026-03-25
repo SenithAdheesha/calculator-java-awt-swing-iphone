e3720f149d81202da1d253ef6dbcb26d9628e54f
Senith Adheesha <senithadeesha7@gmail.com>
2026-03-25 13:21:38 +0530

    Add files via upload

---
+++ Calculator/src/Claculator.java
@@ -0,0 +1,103 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+public class Claculator {
+    int bordwidth = 360;
+    int bordheight = 540;
+
+    Color customLighGray = new Color(212, 212, 210);
+    Color customDarkGray = new Color(80, 80, 80);
+    Color customBlack = new Color(28, 28, 28);
+    Color customOrange = new Color(255, 149, 0);
+
+    String[] buttonValues = {
+            "AC", "+/-", "%", "÷",
+            "7", "8", "9", "×",
+            "4", "5", "6", "-",
+            "1", "2", "3", "+",
+            "0", ".", "√", "="
+
+    };
+
+    String[] rightSymbols = { "÷", "×", "-", "+", "=" };
+    String[] topSymbols = { "AC", "+/-", "%" };
+
+    JFrame frame = new JFrame("Calculator");
+
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
+    JPanel buttonsPanel = new JPanel();
+
+    Claculator() {
+        frame.setVisible(true);
+        frame.setSize(bordwidth, bordheight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(Color.white);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        frame.add(displayPanel, BorderLayout.NORTH);
+
+        buttonsPanel.setLayout(new GridLayout(5, 4));
+        buttonsPanel.setBackground(customBlack);
+        frame.add(buttonsPanel);
+
+        for (int i = 0; i < buttonValues.length; i++) {
+            JButton button = new JButton();
+            String buttonValue = buttonValues[i];
+            button.setFont(new Font("Arial", Font.PLAIN, 30));
+            button.setText(buttonValue);
+            button.setFocusable(false);
+            button.setBorder(new LineBorder(customBlack));
+
+            if (Arrays.asList(topSymbols).contains(buttonValue)) {
+                button.setBackground(customLighGray);
+                button.setForeground(customBlack);
+            } else if (Arrays.asList(rightSymbols).contains(buttonValue)) {
+                button.setBackground(customOrange);
+                button.setForeground(Color.white);
+            } else {
+                button.setBackground(customDarkGray);
+                button.setForeground(Color.white);
+            }
+
+            buttonsPanel.add(button);
+
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    JButton button = (JButton) e.getSource();
+                    String buttonvalue = button.getText();
+
+                    if (Arrays.asList(rightSymbols).contains(buttonValue)) {
+
+                    } else if (Arrays.asList(topSymbols).contains(buttonValue)) {
+
+                    } else {
+                        if (buttonvalue == ".") {
+
+                        } else if ("123456789".contains(buttonValue)) {
+                            if (displayLabel.getText() == "0") {
+                                displayLabel.setText(buttonValue);
+                            } else {
+                                displayLabel.setText(displayLabel.getText() + buttonValue);
+                            }
+                        }
+                    }
+                }
+            });
+
+        }
+    }
+}
